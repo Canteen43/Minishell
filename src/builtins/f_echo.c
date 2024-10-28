1ae1edccad06993fcf21aa6fd8ae2b911b7bc9ef
Karl's WSL <karl.weihmann@gmail.com>
2024-10-28 08:26:10 +0100

    quick-commit

--- src/builtins/f_echo.c
+++ src/builtins/f_echo.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   f_echo.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kweihman <kweihman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/27 17:58:35 by kweihman          #+#    #+#             */
-/*   Updated: 2024/10/28 08:19:14 by kweihman         ###   ########.fr       */
+/*   Created: 2024/10/28 08:19:20 by kweihman          #+#    #+#             */
+/*   Updated: 2024/10/28 08:21:21 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../../inc/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+void	f_echo(int argc, char **argv, char **envp)
 {
-	if (argc == 1)
-		perror("Too few arguments.");
-	if (ft_strcmp(argv[1], "echo") == 0)
-		f_echo(argc, argv, envp);
-	else
-		perror("Command not found.");
-	return (0);
+	int	i;
+
+	i = 2;
+	while (i < argc)
+	{
+		fl_miniprint("%s", argv[i]);
+		if (i + 1 < argc)
+			fl_miniprint(" ");
+		i++;
+	}
+	fl_miniprint("\n");
 }
