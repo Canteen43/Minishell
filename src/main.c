0f4b66cd1d17335585f6e9074a518c27f077d027
Karl Weihmann <kweihman@c3a1c4.42berlin.de>
2024-10-30 14:39:03 +0100

    I am commited

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:58:35 by kweihman          #+#    #+#             */
-/*   Updated: 2024/10/30 14:30:55 by kweihman         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:38:51 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	if (argc == 1)
 		perror("Too few arguments.");
-	if (ft_strcmp(argv[1], "echo") == 0)
+	if (f_strcmp(argv[1], "echo") == 0)
 		f_echo(argc, argv, envp);
 	else
 		perror("Command not found.");
