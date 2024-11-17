bab43516151f8b22d90a080008821f03302479d6
glevin <glev2190@gmail.com>
2024-11-17 17:19:36 +0100

    refactor: using simplified GNL to rm Global vars

--- src/utils/get_next_line/get_next_line.c
+++ src/utils/get_next_line/get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: glevin <glevin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 11:29:50 by glevin            #+#    #+#             */
-/*   Updated: 2024/11/17 13:39:37 by glevin           ###   ########.fr       */
+/*   Updated: 2024/11/17 17:18:56 by glevin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*
+This function determines the index of a new line character within str,
+if there is one. If not, it will return -1.
+*/
+int	ft_istrchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != (unsigned char)c)
+	{
+		if (!str[i++])
+			return (-1);
+	}
+	return (i);
+}
 
 /*
 This function adds the new buffer from the latest read to the end of the stash.
@@ -19,7 +36,7 @@ void	add_to_stash(char **stash, char *buf)
 {
 	char	*tmp_stash;
 
-	tmp_stash = f_strjoin(*stash, buf);
+	tmp_stash = ft_strjoin(*stash, buf);
 	if (tmp_stash == NULL)
 	{
 		free(buf);
@@ -62,27 +79,27 @@ char	*check_new_line(char **stash)
 }
 
 /*
-This one reads the file and saves them to buf. BUFFER_SIZE determines how
+This one reads the file and saves them to buf. buff_size determines how
 many bytes are read from the file on a given call. This function will
-continue reading the file in BUFFER_SIZE size chunks, as the end of the
+continue reading the file in buff_size size chunks, as the end of the
 file hasn't been reached (byte_cnt ==0) and there is no new line character
 in stash. If the end of the file is reached AND there is no new line in
 stash, return stash and free it.
 */
-char	*read_file(int fd, char **stash)
+char	*read_file(int fd, char **stash, int buff_size)
 {
 	char	*buf;
 	int		byte_cnt;
 	char	*next_line;
 
 	byte_cnt = 1;
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buf = (char *)malloc((buff_size + 1) * sizeof(char));
 	if (buf == NULL)
 		return (NULL);
 	next_line = check_new_line(stash);
 	while (!next_line && byte_cnt != 0)
 	{
-		byte_cnt = read(fd, buf, BUFFER_SIZE);
+		byte_cnt = read(fd, buf, buff_size);
 		buf[byte_cnt] = '\0';
 		add_to_stash(stash, buf);
 		next_line = check_new_line(stash);
@@ -100,53 +117,36 @@ char	*read_file(int fd, char **stash)
 /*
 This one is initializing the static char *stash, in order to have access to
 this pointer across multiple function calls. We are checking if the file
-descriptior (FD) and BUFFER_SIZE are valid, or if the file is corrupted.
+descriptor (FD) and buff_size are valid, or if the file is corrupted.
 If all looks kosher, we start reading the file into buffer and begin
 looking for new lines.
-
-BONUS: This approach initializes stash as an array of char
-pointers of length 1024 (an arbitrarily high number for fd). This allows
-the function to handle multiple fd's at once. The pointer to the stash
-corresponding to a given fd, is stored in the 'fd' index of stash.
-Given an fd, the stash[fd] is passed to read_file.
-
-A more storage efficient approach, would be to use a linked list.
-This would allow us to store only the minimum required amount of
-space for handling multiple fds, as appose to immediately initalizing
-a large array of pointers.
 */
-void	handle_new_fd(char **stash)
-{
-	if (*stash == NULL)
-	{
-		*stash = (char *)malloc(1);
-		if (*stash == NULL)
-			return ;
-		(*stash)[0] = '\0';
-	}
-}
 
 char	*get_next_line(int fd)
 {
 	char		*next_line;
-	static char	*stash[1024];
+	static char	*stash;
+	int			buff_size;
 
-	handle_new_fd(&stash[fd]);
-	if (fd < 0 || BUFFER_SIZE <= 0 || 1024 <= fd || read(fd, 0, 0) < 0)
+	buff_size = 10;
+	if (stash == NULL)
 	{
-		free(stash[fd]);
-		stash[fd] = NULL;
+		stash = (char *)malloc(1);
+		if (stash == NULL)
+			return (NULL);
+		stash[0] = '\0';
+	}
+	if (fd < 0 || buff_size <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(stash);
+		stash = NULL;
 		return (NULL);
 	}
-	next_line = read_file(fd, &stash[fd]);
+	next_line = read_file(fd, &stash, buff_size);
+	free(stash);
+	stash = NULL;
 	if (f_strlen(next_line) != 0)
-	{
-		free(stash[fd]);
-		stash[fd] = NULL;
 		return (next_line);
-	}
-	free(stash[fd]);
-	stash[fd] = NULL;
 	free(next_line);
 	return (NULL);
 }
