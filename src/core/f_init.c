d62cae97e178742d9b98c4a4840aa16624704727
Karl <157983509+Canteen43@users.noreply.github.com>
2024-11-19 14:09:48 +0100

    Revert "[WIP] Execution V1"

--- src/core/f_init.c
+++ src/core/f_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   f_init.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: glevin <glevin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:10:08 by kweihman          #+#    #+#             */
-/*   Updated: 2024/11/17 17:21:21 by glevin           ###   ########.fr       */
+/*   Updated: 2024/11/06 14:31:46 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ void	init(t_main *main, char *env[])
 {
 	main->env_head = NULL;
 	main->env_head = f_env_create_lnklst(env);
-	main->gc = NULL;
 	f_handle_signals();
 }
