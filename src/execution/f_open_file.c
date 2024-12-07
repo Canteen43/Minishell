5a40181e9bbc97fabfd4a617f45bd0a796474fed
glevin <glev2190@gmail.com>
2024-12-07 14:17:31 +0100

    refactor: rm extra print statement

--- src/execution/f_open_file.c
+++ src/execution/f_open_file.c
@@ -6,7 +6,7 @@
 /*   By: glevin <glevin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:13:20 by glevin            #+#    #+#             */
-/*   Updated: 2024/12/07 13:46:43 by glevin           ###   ########.fr       */
+/*   Updated: 2024/12/07 14:17:04 by glevin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	f_open_file(t_pipex *pipex, char *filename, int i)
 {
 	int	fd;
 
-	printf("filename: %s\n", filename);
 	fd = 0;
 	if (i == 0)
 		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0777);
