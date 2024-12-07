a18da2a0a82a5b10024c89314bc52de03eb18c75
Canteen43 <karl.weihmann@gmail.com>
2024-12-07 10:39:26 +0100

    fix: deleted newline from exit message. tester now works again

--- src/core/f_free_and_exit.c
+++ src/core/f_free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 12:03:37 by kweihman          #+#    #+#             */
-/*   Updated: 2024/11/27 17:30:35 by kweihman         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:38:24 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	f_free_and_exit(t_main *main, char *message, int code)
 {
 	f_gc_clean(main);
 	if (message)
-		printf("%s, exiting minishell...\n", message);
+		printf("%s, exiting minishell...", message);
 	exit (code);
 }
