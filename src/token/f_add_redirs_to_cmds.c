3910d6e84df1024066c8927fb8b45b7d719bfef3
Canteen43 <karl.weihmann@gmail.com>
2024-12-10 17:40:03 +0100

    started working

--- src/token/f_add_redirs_to_cmds.c
+++ src/token/f_add_redirs_to_cmds.c
@@ -6,40 +6,45 @@
 /*   By: kweihman <kweihman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 14:01:25 by kweihman          #+#    #+#             */
-/*   Updated: 2024/12/09 15:07:42 by kweihman         ###   ########.fr       */
+/*   Updated: 2024/12/10 17:39:04 by kweihman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // Static functions
-static t_tok	*sf_next_command(t_tok *tok);
+static t_tok	*sf_next_command(t_main *main, t_tok *tok);
 static void		sf_add_redir_to_cmd(t_main *main, t_tok *redir_tok,
 					t_tok *cmd_tok);
 
 void	f_add_redirs_to_cmds(t_main *main)
 {
 	t_tok	*tok;
-	t_tok	*tmp;
+	t_tok	*next;
 	t_tok	*current_cmd;
 
 	tok = main->tok_head;
-	current_cmd = sf_next_command(tok);
+	current_cmd = sf_next_command(main, tok);
 	while (tok)
 	{
-		tmp = tok;
-		tok = tok->next;
-		if (tmp->type == PIPE)
-			current_cmd = sf_next_command(tmp);
-		else if (f_tok_is_redir(tmp))
-			sf_add_redir_to_cmd(main, tmp, current_cmd);
+		next = tok->next;
+		if (tok->type == PIPE)
+			current_cmd = sf_next_command(main, tok);
+		else if (f_tok_is_redir(tok))
+			sf_add_redir_to_cmd(main, tok, current_cmd);
+		tok = next;
 	}
 }
 
-static t_tok	*sf_next_command(t_tok *tok)
+static t_tok	*sf_next_command(t_main *main, t_tok *tok)
 {
 	while (tok && tok->type != COMMAND)
 		tok = tok->next;
+	if (tok == NULL)
+	{
+		tok = f_tok_new(main, NULL);
+		tok->type = COMMAND;
+	}
 	return (tok);
 }
 
